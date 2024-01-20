fd5a4c7fe786b1b93d603c9c2c79686b61e9d68a
mishnan07 <mishnanc@gmail.com>
2024-01-20 11:27:03 +0530

    index

--- serverSide/index.js
+++ serverSide/index.js
@@ -5,10 +5,9 @@ import dotenv from 'dotenv'
 import userRoute from './Routes/userRoute.js'
 
 
-
 const app = express()
   
-//
+
 app.use(express.json({limit:'30mb',extended:true}))
 app.use(express.urlencoded({limit:'30mb',extended:true}))
 app.use(cors(
