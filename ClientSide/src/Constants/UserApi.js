6d9fc9b9b87346071bbc0249674dd47077e9215d
mishnan07 <mishnanc@gmail.com>
2024-01-20 11:01:25 +0530

    aaa

--- ClientSide/src/Constants/UserApi.js
+++ ClientSide/src/Constants/UserApi.js
@@ -1,5 +1,6 @@
-// export const userAPI = "http://localhost:3000/";
 
 
 // Update the backend server URL in your frontend code
-export const userAPI = "https://product-apps-api.vercel.app/";
+export const userAPI = "https://product-apps-server.vercel.app/";
+
+// export const userAPI = "http://localhost:3000/";
