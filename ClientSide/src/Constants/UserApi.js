51146ab37f7b370f732fd616646181e687402e2b
mishnan07 <mishnanc@gmail.com>
2024-01-20 09:33:13 +0530

    local

--- ClientSide/src/Constants/UserApi.js
+++ ClientSide/src/Constants/UserApi.js
@@ -1,2 +1,2 @@
-// export const userAPI = "http://localhost:3000/";
-export const userAPI = 'https://product-apps-api.vercel.app/'
+export const userAPI = "http://localhost:3000/";
+// export const userAPI = 'https://product-apps-api.vercel.app/'
